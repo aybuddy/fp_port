087202384cd49c4785c6a0440502617d55703c54
Aaron <ayybuddyy@gmail.com>
2020-03-08 21:49:30 -0400

    Install fullpagejs and set up basic structure

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./sass/main.scss";
+import fullpage from 'fullpage.js';
+
+var myFullPage = new fullpage('#fullPage', {
+    navigation: true,
+    sectionsColor:['#ff5f45', '#0798ec', '#fc6c7c', 'grey']
+});
