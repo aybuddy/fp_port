99d380a4846b236275c59159615a49a4644d9a66
Aaron <ayybuddyy@gmail.com>
2020-03-08 22:55:14 -0400

    Add images, create basic hero page, connect sass imports to base file

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import fullpage from 'fullpage.js';
 
 var myFullPage = new fullpage('#fullPage', {
     navigation: true,
-    sectionsColor:['#ff5f45', '#0798ec', '#fc6c7c', 'grey']
+    autoScrolling: true,
+    // sectionsColor:['#ff5f45', '#0798ec', '#fc6c7c', 'grey']
 });
