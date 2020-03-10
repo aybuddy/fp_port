c8692ac15f068c1bdbfc4dc0d22c48c185a11917
Aaron <ayybuddyy@gmail.com>
2020-03-09 23:02:24 -0400

    Create hero, project, and about sections

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import "./sass/main.scss";
-import fullpage from 'fullpage.js';
+import fullpage from "fullpage.js";
 
-var myFullPage = new fullpage('#fullPage', {
-    navigation: true,
+var myFullPage = new fullpage("#fullPage", {
+    navigation: false,
     autoScrolling: true,
+    scrollingSpeed: 1000,
+    anchors: ["hero", "projects", "about"],
+    slidesNavigation: true,
+    slidesNavPosition: "bottom"
+    // navigationTooltips: ["concordville", "natours"]
+    // fadingEffect: true
     // sectionsColor:['#ff5f45', '#0798ec', '#fc6c7c', 'grey']
 });
